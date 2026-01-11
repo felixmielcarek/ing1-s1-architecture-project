27e58d736c383a326502809dcab0b9add9f264ee
Félix Mielcarek <felix.mielcarek10@gmail.com>
2026-01-11 18:29:35 +0100

    Fix engine problems (always define rotation direction before set the engine on)

--- Sources/main.s
+++ Sources/main.s
@@ -16,8 +16,8 @@
 	IMPORT MOTEUR_GAUCHE_OFF
 	IMPORT MOTEUR_DROIT_OFF
 		
-	IMPORT MOTEUR_DROIT_ARRIERE
-	IMPORT MOTEUR_GAUCHE_ARRIERE
+	IMPORT MOTEUR_DROIT_AVANT
+	IMPORT MOTEUR_GAUCHE_AVANT
 
 	IMPORT ROTATION_90_GAUCHE
 	
@@ -27,25 +27,26 @@
 __main
 	; Initialize motors and bumpers
 	BL MOTEUR_INIT
-	BL BUMPERS_INIT
-	
-	; Turn on both motors
+	;BL BUMPERS_INIT
+
+loop	
+	; 1. Robot goes front
+	BL MOTEUR_GAUCHE_AVANT
+	BL MOTEUR_DROIT_AVANT
 	BL MOTEUR_GAUCHE_ON
 	BL MOTEUR_DROIT_ON
 	
-loop	
-	; 1. Robot goes front
-	BL MOTEUR_DROIT_ARRIERE	   
-	BL MOTEUR_GAUCHE_ARRIERE
-	
 	; 2. When bumpers 1 or 2 or both are activated, rotate 90° left
 check_bumpers
-	BL READ_BUMPERS
-	BNE check_bumpers			; If bumpers NOT pressed (r5!=0), keep checking
+	;BL READ_BUMPERS
+	;BEQ check_bumpers			; If bumpers NOT pressed (r5==0), keep checking
 	
-	; Bumpers pressed (r5==0), handle collision
+	; Bumpers pressed (r5!=0), handle collision
 	; Rotate 90° left
-	BL ROTATION_90_GAUCHE
+	;BL ROTATION_90_GAUCHE
+
+	;BL MOTEUR_GAUCHE_OFF
+	;BL MOTEUR_DROIT_OFF
 	
 	; 3. Robot goes front again (loop back)
 	B loop
